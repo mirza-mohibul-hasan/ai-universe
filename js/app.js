be1c2209dd208d29eb4b937627246276407a803f
Mirza Mohibul Hasan <mirzamohibul618@gmail.com>
2023-03-04 06:52:20 +0600

    Pricing Properly Working

--- js/app.js
+++ js/app.js
@@ -17,6 +17,11 @@ const displayAll = (tools, dataLimit) => {
   }
 
   tools.forEach((tool) => {
+   let list= tool.features;
+   const featureList = list.map((f)=>`<li>${f}</li>`).join('');
+
+// console.log(featureList);
+    
     toolsContainer.innerHTML += `
           <div class="col">
               <div class="card h-100">
@@ -24,10 +29,7 @@ const displayAll = (tools, dataLimit) => {
                 <div class="card-body">
                   <h5 class="card-title">Features</h5>
                   <div id = "features-list">
-                      <p>${tool.features[0] ? "1." + tool.features[0] : ""}</p>
-                      <p>${tool.features[1] ? "2." + tool.features[1] : ""}</p>
-                      <p>${tool.features[2] ? "3." + tool.features[2] : ""}</p>
-                      <p>${tool.features[3] ? "4." + tool.features[3] : ""}</p>
+                    <ol>${featureList}<ol>
                   </div>
                 </div>
                 <div class="card-footer d-flex justify-content-between align-items-center">
@@ -58,7 +60,8 @@ const loadToolDetails = async (id) => {
   displaytoolDetails(data.data);
 };
 const displaytoolDetails = (tool) => {
-  console.log(tool);
+ console.log(Array.isArray(tool.pricing));
+  
   const modalBody = document.getElementById("modal-body");
   modalBody.innerHTML = `
           <div class="row">
@@ -66,15 +69,54 @@ const displaytoolDetails = (tool) => {
             <div class="card border border-danger" style="background: rgba(235, 87, 87, 0.10);">
               <div class="card-body">
                 <h5 class="card-title">${tool.description}</h5>
+                <div id="price-container" class = "card-text d-flex justify-content-between fw-bold" style="gap:5px">
+                
+                <!-- {loadPrice(tool.pricing)} -->
+                <div class="bg-white rounded p-2 text-success d-flex justify-content-center align-items-center" style="flex:1;">
+                    <div>
+                        <p>${(Array.isArray(tool.pricing))? tool.pricing[0]?.price : 'Free of Cost/'}  </p>
+                        <p>${(Array.isArray(tool.pricing))? tool.pricing[0]?.plan :'Basic'}</p>
+                    </div>
+                  </div>
+                  <div class="bg-white rounded p-2 text-warning d-flex justify-content-center align-items-center" style="flex:1;">
+                    <div>
+                        <p>${(Array.isArray(tool.pricing))? tool.pricing[1]?.price : 'Free of Cost/'}  </p>
+                        <p>${(Array.isArray(tool.pricing))? tool.pricing[1]?.plan :'Pro'}</p>
+                    </div>
+                  </div>
+                  <div class="bg-white rounded p-2 text-danger d-flex justify-content-center align-items-center text-center" style="flex:1;">
+                    <div>
+                        <p>${(Array.isArray(tool.pricing))? tool.pricing[2]?.price : 'Free of Cost/'}  </p>
+                        <p>${(Array.isArray(tool.pricing))? tool.pricing[2]?.plan :'Enterprise'}</p>
+                    </div>
+                  </div>
+                  
+                </div>
+                <!-- DownSection End -->
+                <div class="d-flex justify-content-between m-3">
+                    <ul>
+                        <h5 class="card-title">Features</h5>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <ul>
+                        <h5 class="card-title">Integrations</h5>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <!-- DownSection End -->
               </div>
             </div>
           </div>
           <div class="col-sm-6">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <p class="text-end" style="position: absolute;right:1;">Accuracy</p>
+                <img src="${tool.image_link[0]}" alt="" class="img-fluid rounded ">
+                <h5></h5>
               </div>
             </div>
           </div>
